468b26480912d33e92bf072aab90d9d26ce73773
finnmcarthur-tech <finn.mcarthur@student.onslow.school.nz>
2026-04-22 03:07:12 +0000

    the tasks and stuff

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,8 +2,17 @@
 // https://docs.swift.org/swift-book
 
 @main
+// this is a comment
+/* this is also a comment
+a multiline one, in fact */
+// this function should print the String "Hello World" and "this is a new line with my own choice of output" to the terminal
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        print("Hello World")
+        // this print() function is the bit printing "this is a new line with an output of my own choice of output"
+        print("this is a new line with my own choice of output")
+        /* this line will print my initials, drawn using the respective letter
+        for task #1 */
+        print("F F F F F M       M \nF         M M   M M \nF F F     M   M   M \nF         M       M \nF         M       M")
     }
 }
