d9cedd0a0ae06839a9245ad8f7fb5670726dcfd9
finnmcarthur-tech <finn.mcarthur@student.onslow.school.nz>
2026-04-27 21:35:48 +0000

    tasks again (variables and maths, i think)

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -14,5 +14,29 @@ struct SwiftPlayground {
         /* this line will print my initials, drawn using the respective letter
         for task #1 */
         print("F F F F F M       M \nF         M M   M M \nF F F     M   M   M \nF         M       M \nF         M       M")
+
+        //this bit is for the next task(s)
+        //this bit makes a constant of my firstname, and then prints it
+        let firstName = "Finn"
+        print(firstName)
+// this defines my surname and then prints it
+        var surname = "McArthur"
+        print(surname)
+// this changes my surname, then prints it
+        print("Finn dislikes his surname")
+        surname = "Domney"
+        print("\(surname)")
+// this greets me using string interpolation
+        let greeting = "Hello, \(firstName) \(surname)"
+        print(greeting)
+
+        //this is for the task after that
+        let schoolName = "The School Which People Go To For Learning And Things"
+        let schooltime: Double = 762.3
+        var studentName = "John Student"
+        var studentAge: Int128 = 15
+        var studentYear: Int8 = 11
+
+        print("There is a \(studentAge) year old year \(studentYear) student in \(schoolName), who studies for \(schooltime) hours each day.\nTheir name is \(studentName)")
     }
 }
